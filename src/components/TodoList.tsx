b886bac97f7661e38b1a5b313b8698e1e3f55806
BlackSwift <Zamder@ukr.net>
2023-12-02 19:32:44 +0200

    add icons

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./styles.css";
 import { Todo } from "./model";
+import { SingleTodo } from "./SingleTodo";
 
 interface Props {
   todos: Todo[];
@@ -8,5 +9,16 @@ interface Props {
 }
 
 export const TodoList: React.FC<Props> = ({ todos, setTodos }: Props) => {
-  return <div className="todos">TodoList</div>;
+  return (
+    <div className="todos">
+      {todos.map((todo) => (
+        <SingleTodo
+          todo={todo}
+          key={todo.id}
+          todos={todos}
+          setTodos={setTodos}
+        />
+      ))}
+    </div>
+  );
 };
